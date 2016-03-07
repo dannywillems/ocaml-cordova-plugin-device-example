f41f79ce7ce04a8c13511b8f473343763e48d0e2
Danny Willems <contact@danny-willems.be>
2016-03-07 17:44:33 +0100

    Add app directory. The deviceready event isn't executed

---
+++ app/ml/test.ml
@@ -0,0 +1,20 @@
+let on_device_ready _ =
+  let text =
+    "platform: " ^
+    (Js.to_string Device.device##.platform) ^
+    "\nversion: " ^
+    (Js.to_string Device.device##.version) ^
+    "\nuuid: " ^
+    (Js.to_string Device.device##.uuid) ^
+    "\ncordova version: " ^
+    (Js.to_string Device.device##.model) ^
+    "\nmanufacter: " ^
+    (Js.to_string Device.device##.manufacter) ^
+    "\nserial: " ^
+    (Js.to_string Device.device##.serial) in
+  Dom_html.window##alert (Js.string text);
+  Js._true
+
+let _ =
+  Dom.addEventListener Dom_html.document (Dom.Event.make "deviceready")
+(Dom_html.handler on_device_ready) Js._false
